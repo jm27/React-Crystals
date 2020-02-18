161aff283f32c080ed6e88e1dd19a773f3b06e99
Jesus Esquer <jme2791@gmail.com>
2020-02-17 21:01:37 -0700

    changed components to use css grid instead

--- crystal-game/src/App.js
+++ crystal-game/src/App.js
@@ -1,29 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './Components/Header';
+import Main from './Components/Main';
 import './App.css';
-import SimpleContainer from './Components/FluidContainer';
-import CenteredGrid from './Components/CenteredGrid';
 
 function App() {
-  return (
-    <SimpleContainer></SimpleContainer>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+  return(
+  <Header></Header>
+  <Main></Main>
+  )
 }
 
 export default App;
---
+++ crystal-game/src/Components/Header.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function header(){
+    return <h1 className='header'> Crystal Collector </h1>;
+}
