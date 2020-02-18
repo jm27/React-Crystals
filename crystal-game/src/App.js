1c7d982015cd6f7839480760c37ab447553212d1
Jesus Esquer <jme2791@gmail.com>
2020-02-17 21:14:50 -0700

    Added images to main component

--- crystal-game/src/App.js
+++ crystal-game/src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import Header from './Components/Header';
-import Main from './Components/Main';
-import './App.css';
+import React from "react";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import "./App.css";
 
 function App() {
-  return(
-  <Header></Header>
-  <Main></Main>
-  )
+  return (
+    <div>
+      <Header></Header>
+
+      <Main></Main>
+    </div>
+  );
 }
 
 export default App;
--- crystal-game/src/Components/Main.jsx
+++ crystal-game/src/Components/Main.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import CrystalImage from './Crystal';
 
-export default main(){
-    render (
-        <p className='instructions'> Instructions
-        You will be given a random number at the start of the game.
+export default function main() {
+    return (
+        <div>
+            <p className='instructions'> Instructions
+            You will be given a random number at the start of the game.
 
-        There are four crystals below. By clicking on a crystal you will add a specific amount of points to your total score.
+            There are four crystals below. By clicking on a crystal you will add a specific amount of points to your total score.
 
-        You win the game by matching your total score to random number, you lose the game if your total score goes above the random number.
+            You win the game by matching your total score to random number, you lose the game if your total score goes above the random number.
 
-        The value of each crystal is hidden from you until you click on it.
+            The value of each crystal is hidden from you until you click on it.
 
-        Each time when the game starts. the game will change the values of each crystal.
+            Each time when the game starts. the game will change the values of each crystal.
             </p>
+            <div className='images'>
+                <CrystalImage src='https://cdn.shopify.com/s/files/1/0912/7764/articles/blue-crystals-vector-clipart_ed1ae347-444b-4261-9001-1137ae396bab_2048x.png?v=1563439532'></CrystalImage>
+                <CrystalImage src='https://cdn.shopify.com/s/files/1/0912/7764/articles/blue-crystals-vector-clipart_ed1ae347-444b-4261-9001-1137ae396bab_2048x.png?v=1563439532'></CrystalImage>
+                <CrystalImage src='https://cdn.shopify.com/s/files/1/0912/7764/articles/blue-crystals-vector-clipart_ed1ae347-444b-4261-9001-1137ae396bab_2048x.png?v=1563439532'></CrystalImage>
+                <CrystalImage src='https://cdn.shopify.com/s/files/1/0912/7764/articles/blue-crystals-vector-clipart_ed1ae347-444b-4261-9001-1137ae396bab_2048x.png?v=1563439532'></CrystalImage>            
+                </div>
+        </div>
     )
 }
