2efd056d813b0c93bed2ae167c9a433bba82f508
Jesus Esquer <jme2791@gmail.com>
2020-03-18 18:25:15 -0700

    Updates

---
+++ precache-manifest.3d520aabe65277f68d20dc7754f217ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bca23d4e73ce3d199230103b6ad2b3a",
+    "url": "/React-Crystals/index.html"
+  },
+  {
+    "revision": "1ae1caf1dd3b5ac90bca",
+    "url": "/React-Crystals/static/css/main.700a4dee.chunk.css"
+  },
+  {
+    "revision": "3ad28d26ae021e8b0f27",
+    "url": "/React-Crystals/static/js/2.a8b67986.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-Crystals/static/js/2.a8b67986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae1caf1dd3b5ac90bca",
+    "url": "/React-Crystals/static/js/main.80c17c48.chunk.js"
+  },
+  {
+    "revision": "a9fe29800e652cc06a06",
+    "url": "/React-Crystals/static/js/runtime-main.2d9db628.js"
+  },
+  {
+    "revision": "89a9e9c266dbfc96969f58f14aad79d4",
+    "url": "/React-Crystals/static/media/background.89a9e9c2.png"
+  }
+]);
